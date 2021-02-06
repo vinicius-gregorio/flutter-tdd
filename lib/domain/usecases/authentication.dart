48854cd564b0785189bcc8e48dd831705f6e45f3
Vinicius MAC <vincamgreg@hotmail.com>
2021-02-06 19:36:34 -0300

    feat: add Authentication use case

---
+++ lib/domain/usecases/authentication.dart
@@ -0,0 +1,8 @@
+import 'package:meta/meta.dart';
+
+import '../entities/account_entity.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth(
+      {@required String email, @required String password});
+}
