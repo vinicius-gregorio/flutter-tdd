c9d28096a03c61cc03d340a6059447c639ac603c
Vinicius MAC <vincamgreg@hotmail.com>
2021-02-06 21:02:25 -0300

    refactor: move SUT creation to a setup callback method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -22,10 +22,15 @@ abstract class HttpClient {
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
+  HttpClientSpy httpClient;
+  String url;
+  RemoteAuthentication sut;
+  setUp(() {
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
+  });
   test(' Should call HttpClient with correct values', () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
     await sut.auth();
 
     verify(httpClient.request(url: url, method: 'post'));
