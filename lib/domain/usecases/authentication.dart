800cf5c589286175295ab845d012c297ae21f136
Vinicius MAC <vincamgreg@hotmail.com>
2021-02-14 10:43:42 -0300

    refactor: change Authentication case to recieve a model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -3,6 +3,12 @@ import 'package:meta/meta.dart';
 import '../entities/account_entity.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth(
-      {@required String email, @required String password});
+  Future<AccountEntity> auth(AuthenticationParams params);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String secret;
+
+  AuthenticationParams({@required this.email, @required this.secret});
 }
