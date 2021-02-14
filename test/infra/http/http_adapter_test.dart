cbfbb01d4e01b1ce70c1eaf2ffc82acac0eb5b47
Vinicius MAC <vincamgreg@hotmail.com>
2021-02-14 18:01:30 -0300

    feat: ensure HttpAdapter calls post with correct headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -9,7 +9,11 @@ class HttpAdapter {
   HttpAdapter(this.client);
   Future<void> request(
       {@required String url, @required String method, Map body}) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    await client.post(url, headers: headers);
   }
 }
 
@@ -24,7 +28,10 @@ void main() {
 
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(url));
+      verify(client.post(url, headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }));
     });
   });
 }
