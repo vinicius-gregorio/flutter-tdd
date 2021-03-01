c3c6d0a5b5056482acaf298dbf690a5146f9800b
Vinicius MAC <vincamgreg@hotmail.com>
2021-02-28 23:02:27 -0300

    feat: add login ui

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -5,7 +5,64 @@ import '../pages/pages.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Color(0xFF6930c3);
+    final primaryColorDark = Color(0xFF4e2391);
+    final primaryColorLight = Color(0xFF803af0);
+
+    Map<int, Color> color = {
+      50: primaryColor,
+      100: primaryColor,
+      200: primaryColor,
+      300: primaryColor,
+      400: primaryColor,
+      500: primaryColor,
+      600: primaryColor,
+      700: primaryColor,
+      800: primaryColor,
+      900: primaryColor,
+    };
+    MaterialColor customColor = MaterialColor(0xFF6930c3, color);
+
+    final TextStyle headline1 = TextStyle(
+        fontSize: 30, fontWeight: FontWeight.bold, color: primaryColorDark);
+    final TextStyle headline2 = TextStyle(
+        fontSize: 20, fontWeight: FontWeight.bold, color: primaryColorDark);
+    final TextStyle headline3 = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: primaryColorDark,
+    );
     return MaterialApp(
+      theme: ThemeData(
+          primarySwatch: customColor,
+          primaryColor: primaryColor,
+          primaryColorDark: primaryColorDark,
+          primaryColorLight: primaryColorLight,
+          accentColor: primaryColor,
+          backgroundColor: Colors.white,
+          textTheme: TextTheme(headline1: headline1),
+          // buttonTheme: ButtonThemeData(
+          //   buttonColor: primaryColor,
+          //   colorScheme: ColorScheme()
+          // ),
+          textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(
+            textStyle: headline3,
+            shadowColor: Colors.black,
+          )),
+          // colorScheme: ColorScheme.light(primary: primaryColor),
+          // buttonColor: primaryColor,
+          // splashColor: primaryColorLight,
+          // padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          // textTheme: ButtonTextTheme.primary,
+          // shape: RoundedRectangleBorder(
+          //     borderRadius: BorderRadius.circular(20))),
+          inputDecorationTheme: InputDecorationTheme(
+              alignLabelWithHint: true,
+              enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: primaryColorLight)),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: primaryColor)))),
       title: '4Dev',
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -3,39 +3,72 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
+              height: height * 0.35,
+              margin: EdgeInsets.only(bottom: 30),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Theme.of(context).primaryColorLight,
+                      Theme.of(context).primaryColorDark
+                    ]),
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 0),
+                      spreadRadius: 0,
+                      blurRadius: 4,
+                      color: Colors.black),
+                ],
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(80)),
+              ),
               child: Image(image: AssetImage('lib/ui/assets/logo/logo.png')),
             ),
-            Text('Login'.toUpperCase()),
-            Form(
-                child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    icon: Icon(Icons.email),
+            Text('Login'.toUpperCase(),
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline1),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      icon: Icon(
+                        Icons.email,
+                        color: Theme.of(context).primaryColorLight,
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    icon: Icon(Icons.lock),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      icon: Icon(
+                        Icons.lock,
+                        color: Theme.of(context).primaryColorLight,
+                      ),
+                    ),
+                    obscureText: true,
                   ),
-                  obscureText: true,
-                ),
-                TextButton(
-                    onPressed: () {}, child: Text('Entrar'.toUpperCase())),
-                TextButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.person),
-                    label: Text('Criar Conta'))
-              ],
-            )),
+                  Padding(padding: EdgeInsets.symmetric(vertical: 24)),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text('Criar Conta'.toUpperCase())),
+                  TextButton(
+                      onPressed: () {}, child: Text('Entrar'.toUpperCase())),
+                ],
+              )),
+            ),
           ],
         ),
       ),
