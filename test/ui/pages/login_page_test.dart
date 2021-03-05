415716206f90ebc3bc94665c1a0f704b295b187d
Vinicius MAC <vincamgreg@hotmail.com>
2021-03-04 21:56:48 -0300

    feat: ensure LoginPage calls authentication onFormSubmit

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -171,4 +171,16 @@ void main() {
     final button = tester.widget<TextButton>(find.byType(TextButton));
     expect(button.onPressed, null);
   });
+
+  testWidgets('Should call authentication on form submit',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isFormValidController.add(true);
+    await tester.pump();
+    await tester.tap(find.byType(TextButton));
+    await tester.pump();
+
+    verify(presenter.auth()).called(1);
+  });
 }
