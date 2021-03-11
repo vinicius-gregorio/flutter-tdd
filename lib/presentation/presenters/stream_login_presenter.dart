d58bc55da5c9c83340d978b582f9897b163273ae
Vinicius MAC <vincamgreg@hotmail.com>
2021-03-10 21:56:36 -0300

    feat: add ensure StreamLoginPresenter emits password error if validation fails

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -5,6 +5,8 @@ import '../protocols/protocols.dart';
 
 class LoginState {
   String emailError;
+  String passwordError;
+
   bool get isFormValid => false;
 }
 
@@ -16,17 +18,23 @@ class StreamLoginPresenter {
   Stream<String> get emailErrorStream =>
       _controller?.stream?.map((state) => state.emailError)?.distinct();
 
+  Stream<String> get passwordErrorStream =>
+      _controller?.stream?.map((state) => state.passwordError)?.distinct();
+
   Stream<bool> get isFormValidStream =>
       _controller?.stream?.map((state) => state.isFormValid)?.distinct();
 
   StreamLoginPresenter({@required this.validation});
 
+  void _update() => _controller.add(_state);
   void validateEmail(String email) {
     _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
+    _update();
   }
 
   void validatePassword(String password) {
-    _state.emailError = validation.validate(field: 'password', value: password);
+    _state.passwordError =
+        validation.validate(field: 'password', value: password);
+    _update();
   }
 }
