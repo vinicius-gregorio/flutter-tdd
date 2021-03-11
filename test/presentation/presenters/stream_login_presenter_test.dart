5177b5bb7dd176f5b932f0122ed3cf103609d1b6
Vinicius MAC <vincamgreg@hotmail.com>
2021-03-10 22:04:53 -0300

    feat: add ensure StreamLoginPresenter emits password error as null if validation succeeds

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -73,4 +73,15 @@ void main() {
     sut.validatePassword(password);
     sut.validatePassword(password);
   });
+
+  test('Should emit password null if validation fails', () {
+    sut.passwordErrorStream
+        .listen(expectAsync1((error) => expect(error, null)));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validatePassword(password);
+    sut.validatePassword(password);
+  });
 }
